ad98aec4968980ace8f7707d9abeb2af18c18693
SeungHun333 <dvvda456@gmail.com>
2025-05-21 10:56:01 +0900

    feat: Company @Vaild 추가하여 검증 로직 구현, Service 클래스에 @Transactional 추가하여 영속성 관리 추가,

--- src/main/java/com/kbe5/rento/domain/company/controller/CompanyController.java
+++ src/main/java/com/kbe5/rento/domain/company/controller/CompanyController.java
@@ -1,39 +1,41 @@
 package com.kbe5.rento.domain.company.controller;
 
-import com.kbe5.rento.domain.company.dto.request.CompanyDeleteRequest;
 import com.kbe5.rento.domain.company.dto.request.CompanyRegisterRequest;
 import com.kbe5.rento.domain.company.dto.request.CompanyUpdateRequest;
 import com.kbe5.rento.domain.company.dto.response.*;
 import com.kbe5.rento.domain.company.service.CompanyService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/companys")
+@RequestMapping("/api/companies")
 public class CompanyController {
 
     private final CompanyService companyService;
 
     @PostMapping("/register")
-    public ResponseEntity<CompanyRegisterResponse> register(@RequestBody CompanyRegisterRequest request) {
+    public ResponseEntity<CompanyRegisterResponse> register(@RequestBody @Valid CompanyRegisterRequest request) {
         return ResponseEntity.ok(companyService.register(request));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CompanyDetailResponse> getCompanyDetail(@PathVariable Long id) {
+    public ResponseEntity<CompanyResponse> getCompanyDetail(@PathVariable Long id) {
         return ResponseEntity.ok(companyService.getCompanyDetail(id));
     }
 
+    // 추후에 사용 될 메서드 입니다.
     @GetMapping
-    public ResponseEntity<CompanyListResponse> getCompanyList() {
+    public ResponseEntity<List<CompanyResponse>> getCompanyList() {
         return ResponseEntity.ok(companyService.getCompanyList());
     }
 
-    @PutMapping
-    public ResponseEntity<CompanyUpdateResponse> update(@RequestBody CompanyUpdateRequest request) {
-        return ResponseEntity.ok(companyService.update(request));
+    @PutMapping("/{id}")
+    public ResponseEntity<CompanyUpdateResponse> updateCompanyInfo(@RequestBody @PathVariable Long id, @Valid CompanyUpdateRequest request) {
+        return ResponseEntity.ok(companyService.update(id, request));
     }
 }
