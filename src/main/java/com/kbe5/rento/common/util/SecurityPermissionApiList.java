ce20a07cc7302316b4b6274d6ce6cc07b923d429
SeungHun333 <dvvda456@gmail.com>
2025-05-23 15:37:34 +0900

    feat: 추 후 api 권한 관리를 위한 util 클래스 생성

---
+++ src/main/java/com/kbe5/rento/common/util/SecurityPermissionApiList.java
@@ -0,0 +1,70 @@
+package com.kbe5.rento.common.util;
+
+public class SecurityPermissionApiList {
+
+    private SecurityPermissionApiList() {
+    }
+
+    public static final String[] PUBLIC_URLS = {
+
+            "/api/**",
+
+//            // Manager APIs
+//            "/api/managers",
+//            "/api/managers/sign-up",
+//            "/api/managers/login",
+//            "/api/managers/logout",
+//            "/api/managers/{email}",
+//            "/api/managers/{loginId}",
+//            "/api/managers/list/{companyCode}",
+//            "/api/managers/detail/{Id}",
+//
+//            // Member APIs
+//            "/api/members",
+//            "/api/members/{memberId}",
+//
+//            // Vehicle APIs
+//            "/api/vehicles",
+//            "/api/vehicles/{vehicleId}",
+//            "/api/vehicles/{vehicleNumber}",
+//
+//            // Emulator APIs
+//            "/api/emulators/on",
+//            "/api/emulators/off",
+//            "/api/emulators/control-info",
+//            "/api/emulators/control-info/confirm",
+//            "/api/emulators/token",
+//
+//            // Cycle Info APIs
+//            "/api/cycleinfos",
+//
+//            // Geofencing APIs
+//            "/api/geofencings",
+//            "/api/geofencings/{geofencingId}",
+//            "/api/geofencings/send-geo-point",
+//
+//            // Location APIs
+//            "/api/locations",
+//
+//            // Driving History APIs
+//            "/api/driving-histories",
+//            "/api/drivings-histories/{drivinghistory_id}",
+//            "/api/driving-histories?search=",
+//
+//            // Reservation APIs
+//            "/api/reservations",
+//            "/api/reservations/{reservationId}",
+//
+//            // Company APIs
+//            "/api/companies/register",
+//            "/api/companies",
+//            "/api/companies/{companyId}",
+
+            // h2
+            "/h2-console/**",
+
+            // login && signUp
+            "/api/managers/login",
+            "/api/managers/sign-up"
+    };
+}
