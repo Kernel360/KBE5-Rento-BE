767afa76ffb6a52d12534c09dad124600b3fdda9
SeungHun333 <dvvda456@gmail.com>
2025-05-24 15:38:40 +0900

    fix: Security 로그인을 위한 UserDetails, UserDetailService 추가

---
+++ src/main/java/com/kbe5/rento/domain/manager/dto/details/CustomManagerDetails.java
@@ -0,0 +1,56 @@
+package com.kbe5.rento.domain.manager.dto.details;
+
+import com.kbe5.rento.domain.manager.entity.Manager;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+
+@Getter
+public class CustomManagerDetails implements UserDetails {
+
+    private final Manager manager;
+
+    public CustomManagerDetails(Manager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(manager.getRole().getValue()));
+    }
+
+    @Override
+    public String getPassword() {
+        return manager.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return manager.getLoginId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/kbe5/rento/domain/manager/service/CustomMangerDetailsService.java
@@ -0,0 +1,34 @@
+package com.kbe5.rento.domain.manager.service;
+
+import com.kbe5.rento.common.exception.DomainException;
+import com.kbe5.rento.common.exception.ErrorType;
+import com.kbe5.rento.domain.manager.dto.details.CustomManagerDetails;
+import com.kbe5.rento.domain.manager.entity.Manager;
+import com.kbe5.rento.domain.manager.respository.ManagerRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomMangerDetailsService implements UserDetailsService {
+
+    private final ManagerRepository managerRepository;
+
+    public CustomMangerDetailsService(ManagerRepository managerRepository) {
+        this.managerRepository = managerRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Manager manager = managerRepository.findByLoginId(username)
+                .orElseThrow(() -> new DomainException(ErrorType.MANAGER_NOT_FOUND));
+
+        if (manager != null) {
+            return new CustomManagerDetails(manager);
+        }
+
+        return null;
+    }
+}
