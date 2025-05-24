0703c3036933da288436d1ff240a3c513460d2ad
SeungHun333 <dvvda456@gmail.com>
2025-05-24 17:39:23 +0900

    fix: 생성자 주입을 삭제하고 @RequiredArgsConstructor 로 수정

--- src/main/java/com/kbe5/rento/domain/manager/service/CustomMangerDetailsService.java
+++ src/main/java/com/kbe5/rento/domain/manager/service/CustomMangerDetailsService.java
@@ -5,19 +5,17 @@ import com.kbe5.rento.common.exception.ErrorType;
 import com.kbe5.rento.domain.manager.dto.details.CustomManagerDetails;
 import com.kbe5.rento.domain.manager.entity.Manager;
 import com.kbe5.rento.domain.manager.respository.ManagerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomMangerDetailsService implements UserDetailsService {
 
     private final ManagerRepository managerRepository;
-
-    public CustomMangerDetailsService(ManagerRepository managerRepository) {
-        this.managerRepository = managerRepository;
-    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
