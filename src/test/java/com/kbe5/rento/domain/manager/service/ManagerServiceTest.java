c42fa784566393df2c13f98bcfcb089c912d21c7
SeungHun333 <dvvda456@gmail.com>
2025-05-27 14:07:36 +0900

    feat: jwt util test 추가

--- src/test/java/com/kbe5/rento/domain/manager/service/ManagerServiceTest.java
+++ src/test/java/com/kbe5/rento/domain/manager/service/ManagerServiceTest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 import java.util.Optional;
@@ -37,6 +38,9 @@ class ManagerServiceTest {
 
     @Mock
     private ManagerRepository managerRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     Manager manager;
     Company company;
