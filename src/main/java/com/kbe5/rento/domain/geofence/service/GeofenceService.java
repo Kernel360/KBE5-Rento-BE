30e4175073a54297c3822f2a172bdae9474384bd
SeungHun333 <dvvda456@gmail.com>
2025-06-02 11:03:02 +0900

    feat: 지오펜스 서비스, DB 접근 로직 구현

---
+++ src/main/java/com/kbe5/rento/domain/geofence/service/GeofenceService.java
@@ -0,0 +1,51 @@
+package com.kbe5.rento.domain.geofence.service;
+
+import com.kbe5.rento.common.exception.DomainException;
+import com.kbe5.rento.common.exception.ErrorType;
+import com.kbe5.rento.domain.geofence.entity.Geofence;
+import com.kbe5.rento.domain.geofence.repository.GeofenceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GeofenceService {
+
+    private final GeofenceRepository geofenceRepository;
+
+    public void register(Geofence geofence) {
+        geofenceRepository.save(geofence);
+    }
+
+    public List<Geofence> getGeofenceList(String companyCode) {
+        List<Geofence> geofenceList = geofenceRepository.findAllByCompanyCode(companyCode);
+
+        if (geofenceList.isEmpty()) {
+            throw new DomainException(ErrorType.GEOFENCE_NOT_FOUND);
+        }
+
+        return geofenceList;
+    }
+
+    public Geofence getGeofenceDetail(Long id) {
+
+        return geofenceRepository.findById(id)
+                .orElseThrow(() -> new DomainException(ErrorType.COMPANY_NOT_FOUND));
+    }
+
+    public void delete(Long id) {
+        Geofence geofence = geofenceRepository.findById(id)
+                        .orElseThrow(() -> new DomainException(ErrorType.GEOFENCE_NOT_FOUND));
+
+        geofenceRepository.delete(geofence);
+    }
+
+    public void update(Long id, Geofence updatedGeofence) {
+        Geofence geofence = geofenceRepository.findById(id)
+                .orElseThrow(() -> new DomainException(ErrorType.GEOFENCE_NOT_FOUND));
+
+        geofence.toUpdate(updatedGeofence);
+    }
+}
+
