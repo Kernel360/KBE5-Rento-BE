de346812ae1e6043291c62b72233ae008a97a118
SeungHun333 <dvvda456@gmail.com>
2025-06-03 17:07:11 +0900

    feat: 토큰 생성 시 필요한 Dto 추가

---
+++ src/main/java/com/kbe5/rento/common/jwt/dto/JwtManagerArgumentDto.java
@@ -0,0 +1,27 @@
+package com.kbe5.rento.common.jwt.dto;
+
+import com.kbe5.rento.common.jwt.util.JwtUtil;
+import com.kbe5.rento.domain.manager.entity.Manager;
+
+public record JwtManagerArgumentDto(
+        Long id,
+        String loginId,
+        Long companyId,
+        String name,
+        String phone,
+        String email,
+        String companyCode,
+        String role
+) {
+    public static JwtManagerArgumentDto fromEntity(Manager manager) {
+        return new JwtManagerArgumentDto(manager.getId(), manager.getLoginId(), manager.getCompany().getId(),
+                manager.getName(), manager.getPhone(), manager.getEmail(), manager.getCompanyCode(),
+                manager.getRole().toString());
+    }
+
+    public static JwtManagerArgumentDto of(JwtUtil util, String token) {
+        return new JwtManagerArgumentDto(util.getId(token), util.getLoginId(token),
+                util.getCompanyId(token), util.getName(token), util.getPhone(token),
+                util.getEmail(token), util.getCompanyCode(token), util.getRole(token));
+    }
+}
