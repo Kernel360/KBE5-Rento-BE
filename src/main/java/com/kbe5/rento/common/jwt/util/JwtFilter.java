4a7fe9a7c2ffe839187a4bd286ead6294273b471
SeungHun333 <dvvda456@gmail.com>
2025-06-04 10:44:00 +0900

    fix: Error Response로 내려주는 방식으로 수정

--- src/main/java/com/kbe5/rento/common/jwt/util/JwtFilter.java
+++ src/main/java/com/kbe5/rento/common/jwt/util/JwtFilter.java
@@ -1,5 +1,6 @@
 package com.kbe5.rento.common.jwt.util;
 
+import com.kbe5.rento.common.exception.DomainException;
 import com.kbe5.rento.common.exception.ErrorType;
 import com.kbe5.rento.domain.manager.dto.details.CustomManagerDetails;
 import com.kbe5.rento.domain.manager.respository.ManagerRepository;
@@ -30,33 +31,37 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request,@NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String accessToken = request.getHeader("AccessToken");
 
-        String accessToken = request.getHeader("AccessToken");
+            if (accessToken == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
 
-        if (accessToken == null) {
-            filterChain.doFilter(request, response);
-            return;
+            if (jwtUtil.isExpired(accessToken)) {
+                return;
+            }
+
+            String category = jwtUtil.getCategory(accessToken);
+
+            if (!category.equals("access")) {
+                DomainException domainException = new DomainException(ErrorType.INVALID_TOKEN);
+                jwtUtil.tokenErrorResponse(response, domainException);
+                return;
+            }
+
+            CustomManagerDetails customManagerDetails =
+                    (CustomManagerDetails) new CustomMangerDetailsService(managerRepository)
+                            .loadUserByUsername(jwtUtil.getLoginId(accessToken));
+
+            Authentication authToken = new UsernamePasswordAuthenticationToken(
+                    customManagerDetails, null, customManagerDetails.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        } catch (DomainException e) {
+            jwtUtil.tokenErrorResponse(response, e);
         }
-
-        if (jwtUtil.isExpired(accessToken)) {
-            return;
-        }
-
-        String category = jwtUtil.getCategory(accessToken);
-
-        if (!category.equals("access")) {
-            jwtUtil.tokenErrorResponse(response, ErrorType.INVALID_TOKEN);
-            return;
-        }
-
-        CustomManagerDetails customManagerDetails =
-                (CustomManagerDetails) new CustomMangerDetailsService(managerRepository)
-                        .loadUserByUsername(jwtUtil.getLoginId(accessToken));
-
-        Authentication authToken = new UsernamePasswordAuthenticationToken(
-                customManagerDetails, null, customManagerDetails.getAuthorities());
-
-        SecurityContextHolder.getContext().setAuthentication(authToken);
 
         filterChain.doFilter(request, response);
     }
