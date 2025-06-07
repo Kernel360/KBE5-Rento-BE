43dff29d1b647bcd9e7df99010a4c34c20416c3c
SeungHun333 <dvvda456@gmail.com>
2025-06-07 21:05:09 +0900

    fix: GeofenceEvent 컨트롤러, 레포지토리, Entity, DTO 추가

---
+++ src/main/java/com/kbe5/rento/domain/event/entity/GeofenceEvent.java
@@ -0,0 +1,28 @@
+package com.kbe5.rento.domain.event.entity;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Getter
+@Entity
+@SuperBuilder
+@DiscriminatorValue("GEOFENCE")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GeofenceEvent extends Event {
+
+    LocalDateTime oTime;
+
+    int geoGrpId;
+
+    int geoPid;
+
+    int evtVal;
+}
