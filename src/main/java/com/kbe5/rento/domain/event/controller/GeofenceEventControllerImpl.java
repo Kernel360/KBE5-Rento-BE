8f3a1bcdccbcc0f4660b298c4562250c87c71e3c
SeungHun333 <dvvda456@gmail.com>
2025-06-12 10:25:56 +0900

    fix: Geofence Event RabbitMQ 적용한 Handler 방식으로 수정

--- src/main/java/com/kbe5/rento/domain/event/controller/GeofenceEventControllerImpl.java
+++ src/main/java/com/kbe5/rento/domain/event/controller/GeofenceEventControllerImpl.java
@@ -1,36 +1,36 @@
-//package com.kbe5.rento.domain.event.controller;
-//
-//import com.kbe5.rento.domain.device.entity.DeviceToken;
-//import com.kbe5.rento.domain.device.enums.DeviceResultCode;
-//import com.kbe5.rento.domain.event.dto.request.geofence.GeofenceEventRequest;
-//import com.kbe5.rento.domain.event.dto.response.EventResponse;
-//import com.kbe5.rento.domain.event.entity.GeofenceEvent;
-//import com.kbe5.rento.domain.event.service.EventService;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.validation.annotation.Validated;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@Slf4j
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/api/events/geofences")
-//public class GeofenceEventControllerImpl implements GeofenceEventController{
-//
-//    private final EventService eventService;
-//
-//    @PostMapping
-//    public ResponseEntity<EventResponse> receiveGeofenceEvent (@AuthenticationPrincipal DeviceToken deviceToken,
-//                                                 @RequestBody @Validated GeofenceEventRequest request) {
-//
-//        GeofenceEvent geofenceEvent = request.toEntity(deviceToken.getDeviceId());
-//        GeofenceEvent savedEvent = eventService.ignitionOnEvent(geofenceEvent, deviceToken);
-//
-//        return ResponseEntity.ok(EventResponse.fromEntity(DeviceResultCode.SUCCESS, savedEvent));
-//    }
-//}
+package com.kbe5.rento.domain.event.controller;
+
+import com.kbe5.rento.domain.device.entity.DeviceToken;
+import com.kbe5.rento.domain.device.enums.DeviceResultCode;
+import com.kbe5.rento.domain.event.amqp.EventSender;
+import com.kbe5.rento.domain.event.dto.request.geofence.GeofenceEventRequest;
+import com.kbe5.rento.domain.event.dto.response.EventResponse;
+import com.kbe5.rento.domain.event.entity.GeofenceEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/events/geofences")
+public class GeofenceEventControllerImpl implements GeofenceEventController{
+
+    private final EventSender eventSender;
+
+    @PostMapping
+    public ResponseEntity<EventResponse> receiveGeofenceEvent (@AuthenticationPrincipal DeviceToken deviceToken,
+                                                 @RequestBody @Validated GeofenceEventRequest request) {
+
+        GeofenceEvent geofenceEvent = request.toEntity(deviceToken.getDeviceId());
+        eventSender.send(geofenceEvent, request.mdn());
+
+        return ResponseEntity.ok(EventResponse.fromEntity(DeviceResultCode.SUCCESS, request.mdn()));
+    }
+}
