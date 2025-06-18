878784df492dba34f3db36890dad287f4e8ec4c3
soyun <ehouse16@naver.com>
2025-06-18 18:26:35 +0900

    fix: 사용하지 않는 import 삭제

--- src/main/java/com/kbe5/rento/domain/firebase/dto/TokenNotificationRequest.java
+++ src/main/java/com/kbe5/rento/domain/firebase/dto/TokenNotificationRequest.java
@@ -1,13 +1,8 @@
 package com.kbe5.rento.domain.firebase.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 public record TokenNotificationRequest(
     String title,
     String content,
-    //todo: 관련된 url을 첨부할 것인지?
     String url
 ) {
 }
