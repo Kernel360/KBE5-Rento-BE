9f07809bcef9b09ce1f850fdeb20c39c34c9af2b
ugiuk00 <ugiuk00@naver.com>
2025-06-20 18:27:51 +0900

    refactor: adapter에서 domain 빈 활성화

--- rento-adapter/src/main/java/com/kbe5/adapter/AdapterApplication.java
+++ rento-adapter/src/main/java/com/kbe5/adapter/AdapterApplication.java
@@ -2,8 +2,12 @@ package com.kbe5.adapter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.kbe5.domain")
+@EntityScan(basePackages = "com.kbe5.domain")
 class AdapterApplication {
 
     public static void main(String[] args) {
