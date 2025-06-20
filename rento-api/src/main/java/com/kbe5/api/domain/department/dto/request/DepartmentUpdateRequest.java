38f895b74e2a50659dc7189d75ec9b2afe16eed6
ugiuk00 <ugiuk00@naver.com>
2025-06-20 17:34:38 +0900

    refactor: department관련 클래스 분리

---
+++ rento-api/src/main/java/com/kbe5/api/domain/department/dto/request/DepartmentUpdateRequest.java
@@ -0,0 +1,11 @@
+package com.kbe5.api.domain.department.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DepartmentUpdateRequest(
+        @NotBlank(message = "기업코드는 필수입니다.")
+        String companyCode,
+        @NotBlank(message = "부서이름은 필수입니다.")
+        String departmentName
+) {
+}
