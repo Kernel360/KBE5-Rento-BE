4b5f23a3bde97075a8a0d2ae326f464a8071575b
JangBJ <qudwnd1001@naver.com>
2025-06-22 13:26:21 +0900

    🛠️ refactor: 코멘트 반영

--- src/main/java/com/kbe5/rento/domain/cycleInfoSummary/controller/CycleInfoSummaryController.java
+++ src/main/java/com/kbe5/rento/domain/cycleInfoSummary/controller/CycleInfoSummaryController.java
@@ -1,8 +1,12 @@
 package com.kbe5.rento.domain.cycleInfoSummary.controller;
 
+import com.kbe5.rento.common.apiresponse.ResEntityFactory;
+import com.kbe5.rento.common.response.api.ApiResponse;
+import com.kbe5.rento.common.response.api.ApiResultCode;
 import com.kbe5.rento.domain.cycleInfoSummary.dto.CycleInfoSummaryResponse;
 import com.kbe5.rento.domain.cycleInfoSummary.service.CycleInfoSummaryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,7 +19,7 @@ public class CycleInfoSummaryController {
     private final CycleInfoSummaryService cycleInfoSummaryService;
 
     @GetMapping("/{driveId}")
-    public List<CycleInfoSummaryResponse> getList(@PathVariable Long driveId){
-        return cycleInfoSummaryService.getList(driveId);
+    public ResponseEntity<ApiResponse<List<CycleInfoSummaryResponse>>> getList(@PathVariable Long driveId){
+        return ResEntityFactory.toResponse(ApiResultCode.SUCCESS, cycleInfoSummaryService.getList(driveId));
     }
 }
