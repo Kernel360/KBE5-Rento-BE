aa97e2044aa94ca449d910c9b8128e695410fded
SeungHun333 <dvvda456@gmail.com>
2025-06-24 16:09:19 +0900

    fix: JWT 민감 정보 마스킹 처리

--- rento-api/src/main/java/com/kbe5/api/domain/manager/dto/request/ManagerSignUpRequest.java
+++ rento-api/src/main/java/com/kbe5/api/domain/manager/dto/request/ManagerSignUpRequest.java
@@ -3,9 +3,11 @@ package com.kbe5.api.domain.manager.dto.request;
 import com.kbe5.domain.manager.entity.Manager;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record ManagerSignUpRequest(
         @NotBlank
+        @Size(min = 4, message = "아이디는 4자리 이상이어야합니다.")
         String loginId,
 
         @NotBlank
