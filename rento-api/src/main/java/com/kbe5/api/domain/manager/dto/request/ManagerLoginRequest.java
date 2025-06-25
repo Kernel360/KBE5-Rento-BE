fc43584325ad2f04da8a22e1b7bbec75770ce588
SeungHun333 <dvvda456@gmail.com>
2025-06-25 16:45:43 +0900

    feat: @Notblank 적용

--- rento-api/src/main/java/com/kbe5/api/domain/manager/dto/request/ManagerLoginRequest.java
+++ rento-api/src/main/java/com/kbe5/api/domain/manager/dto/request/ManagerLoginRequest.java
@@ -1,7 +1,13 @@
 package com.kbe5.api.domain.manager.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ManagerLoginRequest(
+
+        @NotBlank
         String loginId,
+
+        @NotBlank
         String password
 ) {
 }
