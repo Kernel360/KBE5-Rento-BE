a6b2b6defd3c3368391e5fb1c3d95ae7bda8de27
SeungHun333 <dvvda456@gmail.com>
2025-06-25 16:39:06 +0900

    feat: payload에 저장되는 민감 정보 암호화를 위한 암호화 util 클래스 생성

---
+++ common/src/main/java/com/kbe5/common/util/Aes256Util.java
@@ -0,0 +1,52 @@
+package com.kbe5.common.util;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+
+@Component
+public class Aes256Util {
+
+    private final String secretKey;
+    private final String IV;
+
+    public Aes256Util(@Value("${AES_KEY}") String secretKey) {
+        this.secretKey = secretKey;
+        this.IV = secretKey.substring(0, 16);
+    }
+
+    public String AES_Encode(String plainText) {
+        try {
+            SecretKey secureKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+            cipher.init(Cipher.ENCRYPT_MODE, secureKey, new IvParameterSpec(IV.getBytes(StandardCharsets.UTF_8)));
+            byte[] encrypted = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
+
+            return Base64.encodeBase64String(encrypted);
+        } catch (GeneralSecurityException e) {
+            throw new RuntimeException("암호화 중 오류 발생", e);
+        }
+    }
+
+    public String AES_Decode(String cipherText) {
+        try {
+            SecretKey secureKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+            cipher.init(Cipher.DECRYPT_MODE, secureKey, new IvParameterSpec(IV.getBytes(StandardCharsets.UTF_8)));
+            byte[] decrypted = cipher.doFinal(Base64.decodeBase64(cipherText));
+
+            return new String(decrypted, StandardCharsets.UTF_8);
+        } catch (GeneralSecurityException e) {
+            throw new RuntimeException("복호화 중 오류 발생", e);
+        }
+    }
+}
