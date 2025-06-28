4abd2ee6555675ea103bd78e6d44fe7a3baeeee0
soyun <ehouse16@naver.com>
2025-06-28 16:28:27 +0900

    refactor: `@Retryable` 설정 추가

--- rento-adapter/src/main/java/com/kbe5/adapter/AdapterApplication.java
+++ rento-adapter/src/main/java/com/kbe5/adapter/AdapterApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication(scanBasePackages = {
         "com.kbe5.common",
         "com.kbe5.domain",
