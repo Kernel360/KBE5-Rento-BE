2565a911070b90490f431fc9e61c862d6b12cf1f
soyun <ehouse16@naver.com>
2025-07-05 17:47:44 +0900

    refactor: department DDD로 수정
    # Conflicts:
    #	domain/src/main/java/com/kbe5/domain/department/dto/DepartmentCommand.java
    #	domain/src/main/java/com/kbe5/domain/department/service/DepartmentService.java
    #	rento-api/src/main/java/com/kbe5/api/domain/department/controller/DepartmentControllerImpl.java
    #	rento-api/src/main/java/com/kbe5/api/domain/department/dto/request/DepartmentRegisterRequest.java
    #	rento-api/src/main/java/com/kbe5/api/domain/department/dto/response/DepartmentInfoResponse.java
    #	rento-api/src/main/java/com/kbe5/api/domain/member/service/MemberService.java

--- rento-api/src/main/java/com/kbe5/api/domain/department/dto/request/DepartmentUpdateRequest.java
+++ rento-api/src/main/java/com/kbe5/api/domain/department/dto/request/DepartmentUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.kbe5.api.domain.department.dto.request;
 
+import com.kbe5.domain.department.dto.DepartmentCommand;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -11,4 +12,10 @@ public record DepartmentUpdateRequest(
         @Size(max = 10, message = "부서이름은 10자 이내여야 합니다.")
         String departmentName
 ) {
+        public DepartmentCommand.Update toCommand() {
+                return DepartmentCommand.Update.builder()
+                        .companyId(companyId)
+                        .departmentName(departmentName)
+                        .build();
+        }
 }
