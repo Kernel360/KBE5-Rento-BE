26420d6cb723f69ec8570f0a412f648449459897
soyun <ehouse16@naver.com>
2025-07-05 19:58:05 +0900

    refactor: department Mapper 생성

--- rento-api/src/main/java/com/kbe5/api/domain/department/dto/request/DepartmentUpdateRequest.java
+++ rento-api/src/main/java/com/kbe5/api/domain/department/dto/request/DepartmentUpdateRequest.java
@@ -1,6 +1,5 @@
 package com.kbe5.api.domain.department.dto.request;
 
-import com.kbe5.domain.department.dto.DepartmentCommand;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -12,10 +11,4 @@ public record DepartmentUpdateRequest(
         @Size(max = 10, message = "부서이름은 10자 이내여야 합니다.")
         String departmentName
 ) {
-        public DepartmentCommand.Update toCommand() {
-                return DepartmentCommand.Update.builder()
-                        .companyId(companyId)
-                        .departmentName(departmentName)
-                        .build();
-        }
 }
