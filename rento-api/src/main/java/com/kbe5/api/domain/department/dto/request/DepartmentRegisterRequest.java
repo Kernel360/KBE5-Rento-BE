4e954d55682797c9905f3f0538d0ac0663b5b6dc
soyun <ehouse16@naver.com>
2025-07-05 19:58:05 +0900

    refactor: department Mapper 생성

--- rento-api/src/main/java/com/kbe5/api/domain/department/dto/request/DepartmentRegisterRequest.java
+++ rento-api/src/main/java/com/kbe5/api/domain/department/dto/request/DepartmentRegisterRequest.java
@@ -1,6 +1,5 @@
 package com.kbe5.api.domain.department.dto.request;
 
-import com.kbe5.domain.department.dto.DepartmentCommand;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -9,11 +8,4 @@ public record DepartmentRegisterRequest(
         @Size(max = 10, message = "부서이름은 10자 이내여야 합니다.")
         String departmentName
 ) {
-
-        public DepartmentCommand.Register toCommand(Long companyId){
-                return DepartmentCommand.Register.builder()
-                        .departmentName(departmentName)
-                        .companyId(companyId)
-                        .build();
-        }
 }
--- rento-api/src/main/java/com/kbe5/api/domain/department/dto/response/DepartmentInfoResponse.java
+++ rento-api/src/main/java/com/kbe5/api/domain/department/dto/response/DepartmentInfoResponse.java
@@ -1,27 +1,9 @@
 package com.kbe5.api.domain.department.dto.response;
 
 
-import com.kbe5.domain.department.dto.DepartmentInfo;
-import com.kbe5.domain.department.entity.Department;
-
 public record DepartmentInfoResponse(
         Long departmentId,
         String departmentName,
         int numberOfEmployees
 ) {
-    public static DepartmentInfoResponse fromEntity(Department department) {
-        return new DepartmentInfoResponse(
-                department.getId(),
-                department.getDepartmentName(),
-                department.getNumberOfEmployee()
-        );
-    }
-
-    public static DepartmentInfoResponse fromDepartmentInfo(DepartmentInfo departmentInfo) {
-        return new DepartmentInfoResponse(
-                departmentInfo.getDepartmentId(),
-                departmentInfo.getDepartmentName(),
-                departmentInfo.getNumberOfEmployees()
-        );
-    }
 }
