06baeb3696eecaafa9b8362e522ab95c34b3a0ab
sean <tmdgus717@naver.com>
2025-07-09 16:50:18 +0900

    DriveService merger 해결

--- domain/src/main/java/com/kbe5/domain/drive/service/DriveService.java
+++ domain/src/main/java/com/kbe5/domain/drive/service/DriveService.java
@@ -1,12 +1,5 @@
 package com.kbe5.domain.drive.service;
 
-<<<<<<< HEAD
-import java.time.LocalDateTime;
-
-public interface DriveService {
-
-    Long findDriveForEvent(Long mdn, LocalDateTime now);
-=======
 import com.kbe5.domain.drive.dto.DriveAddCommand;
 import com.kbe5.domain.drive.dto.DriveInfo;
 import com.kbe5.domain.manager.entity.Manager;
@@ -34,5 +27,4 @@ public interface DriveService {
             Manager manager,
             String vehicleNumber
     );
->>>>>>> 9e9ed5b78eeefed57edf0ccb6f562520c1c429e8
 }
