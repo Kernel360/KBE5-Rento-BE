3f14e27f35343dfbb7c91579daadd8051e82c56f
sean <tmdgus717@naver.com>
2025-07-17 18:13:16 +0900

    sub count 추가

--- rento-sub/src/main/java/com/kbe5/sub/amqp/EventReceiver.java
+++ rento-sub/src/main/java/com/kbe5/sub/amqp/EventReceiver.java
@@ -16,14 +16,16 @@ import java.io.IOException;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-@RabbitListener(queues = {"cycle-info-1", "cycle-info-2", "cycle-info-3"}, concurrency = "6")
+@RabbitListener(queues = {"cycle-info-1", "cycle-info-2", "cycle-info-3"}, concurrency = "12")
 public class EventReceiver {
 
+    public static int count = 0;
     private final EventService eventService;
 
     @RabbitHandler
     public void receive(Event event, @Header(AmqpHeaders.CONSUMER_QUEUE) String queueName) throws IOException {
         log.info("Received event from queue {}: {}", queueName, event);
+        log.info("receiver count : {}", count++);
         eventService.processEvent(event);
     }
 }
